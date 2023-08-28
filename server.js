25c301478fb32fec104bb808d02e5edbde1ef92b
salamanderctesiphon <swbrookshire@gmail.com>
2023-08-28 16:01:18 -0500

    sockets io is being blocked by my browser

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
+const path = require("path");
+const http = require("http");
 const express = require("express");
-const app = express();
+const socketio = require("socket.io");
 
-const PORT = 3000 || process.env.PORT;
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+// set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// run when a client conects
+io.on('connection', socket => {
+  console.log('New WS Connection...');
+});
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
